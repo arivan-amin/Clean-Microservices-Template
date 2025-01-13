007e69b6cacdc1c45e1bb5209c732b7318fce673
Arivan-Amin <arivanamin@gmail.com>
2025-01-13 15:52:35 +0300

    Add TestContainers to testing module

---
+++ Testing/src/main/java/com/arivanamin/app/name/backend/testing/architecture/bases/BaseMongoDatabaseTest.java
@@ -0,0 +1,21 @@
+package com.arivanamin.app.name.backend.testing.architecture.bases;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public interface BaseMongoDatabaseTest extends BaseIntegrationTest {
+    
+    @Container
+    MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer("mongo:8.0.0");
+    
+    @DynamicPropertySource
+    static void registerProperties (DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", MONGO_DB_CONTAINER::getReplicaSetUrl);
+    }
+}
--- Testing/src/main/java/com/arivanamin/app/name/backend/testing/architecture/bases/BaseUnitTest.java
+++ Testing/src/main/java/com/arivanamin/app/name/backend/testing/architecture/bases/BaseUnitTest.java
@@ -2,7 +2,10 @@ package com.arivanamin.app.name.backend.testing.architecture.bases;
 
 import com.github.javafaker.Faker;
 import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith (MockitoExtension.class)
 public interface BaseUnitTest {
     
     Faker FAKER = new Faker();
